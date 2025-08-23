3aec95886eab96d6febe91c50ee8728e7461bd74
Zaid Mohammed Zainudeen <zainudeenzaidmohammed7@gmail.com>
2025-08-23 17:37:22 +0000

    Connected auth to backend

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,23 @@
 // app/index.jsx
 import { Redirect } from "expo-router";
+import { ActivityIndicator, View } from "react-native";
 import { useAuth } from "../context/AuthContext";
 
 export default function Index() {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
+
+  // show a loader while checking storage
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" color="#000" />
+      </View>
+    );
+  }
 
   // if logged in → go to tabs, else → login
   if (user) {
-    return <Redirect href="/(tabs)" />;
+    return <Redirect href="/(tabs)/home" />;
   } else {
     return <Redirect href="/auth/login" />;
   }
